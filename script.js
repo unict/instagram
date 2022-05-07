49817e09413ebc2dc2ff281b885364e77ecb5df7
unict <roparkj2@nate.com>
2022-05-07 21:56:18 +0900

    js & DOM

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-///console.log("Java Scrip Test")
-//alert("오늘 기분 어때?")
+//P320
+document.addEventListener("DOMContentLoaded",
+    function(e){
+        let button=document.querySelector("input")
+        button.addEventListener("input",
+            function(e){
+                console.log(e.target.value)
+            })
+    }
+)
+//let button=document.querySelector("input")
+//button.addEventListener("input",
+//function(e){
+//        console.log(e.target.value)
+//    }
+//)
 function numbering(n){
     for(let i=0;i<n;i++){
         console.log(i)
         }
     }
-let button=document.querySelector("input")
-button.addEventListener("input",
-    function(e){
-        console.log(e.target.value)
-    }
-)
