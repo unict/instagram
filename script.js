da5c29fb0e956100f47b252592fa422b9de42f7d
unict <roparkj2@nate.com>
2022-05-07 23:21:34 +0900

    js & DOM & Function

--- script.js
+++ script.js
@@ -19,3 +19,15 @@ function numbering(n){
         console.log(i)
         }
     }
+
+function doSomething(){
+    let a=document.getElementById('InputA').value;
+    let b=document.getElementById('InputB').value;
+    document.getElementById('valueA').innerHTML=a;
+    document.getElementById('valueB').innerHTML=b;
+    document.getElementById('valueC').innerHTML=Number(a)+Number(b);
+}
+
+function whatTimeIsIt(){
+    alert(new Date());
+}
