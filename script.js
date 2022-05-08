eaba5e36dd87c0fa9b61a74a174bda07fc23dae4
unict <roparkj2@nate.com>
2022-05-08 21:04:34 +0900

    bootstrap event

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 //P320
 document.addEventListener("DOMContentLoaded",
     function(e){
-        let button=document.querySelector("input")
-        button.addEventListener("input",
+        //let button=document.querySelector("input")
+        //button.addEventListener("input",
+        //    function(e){
+        //        console.log(e.target.value)
+        //    })
+        
+        //도큐먼트에 이벤트 추가
+        document.addEventListener("keydown",
+            function(e){
+                console.log(e.key) //콘솔에서 입력되는 키 확인
+                if (e.key === "q"){
+                    alert("this is Q")
+                }
+            }
+        )
+
+        //일반개체에 이벤트 추가
+        let keydown=document.querySelector("#keydown")
+        keydown.addEventListener("keydown",
+            function(e){
+                console.log(e.target.value)
+            })
+        let change=document.querySelector("#change")
+        change.addEventListener("change",
+            function(e){
+                console.log(e.target.value)
+            })
+        let input=document.querySelector("#input")
+        input.addEventListener("input",
             function(e){
                 console.log(e.target.value)
             })
@@ -30,4 +57,12 @@ function doSomething(){
 
 function whatTimeIsIt(){
     alert(new Date());
+}
+
+function red(e){
+    e.target.style.backgroundColor="red"
+}
+
+function blue(e){
+    e.target.style.backgroundColor="blue"
 }
